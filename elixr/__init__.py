78ed900f4ff3eb8f410ab8763e28cbd27e143981
hkmshb <hkmshb@gmail.com>
2017-02-21 18:27:54 +0100

    Bumped version to 0.1.

--- elixr/__init__.py
+++ elixr/__init__.py
@@ -5,7 +5,7 @@ Copyright (c) 2017, Hazeltek Solutions.
 """
 
 __author__  = 'Hazeltek Solutions'
-__version__ = '0.0'
+__version__ = '0.1'
 
 
 # expose significant functions and classes defined within the core module
